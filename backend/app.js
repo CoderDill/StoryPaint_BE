baf55e5eef7ec89e6ad284e2c0e5126e770a1d78
Matthew Dillon <mddillon47@gmail.com>
2021-11-22 17:04:40 -0800

    first commit

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
+const app = express();
+
+const cRouter = require("./routes/colorings");
+const uRouter = require("./routes/users");
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
+app.use(logger("dev"));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
+
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "build")));
+app.use(cors());
+app.use("/users", uRouter);
+app.use("/colorings", cRouter);
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, "build", "index.html");
+  res.sendFile(index);
+});
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
